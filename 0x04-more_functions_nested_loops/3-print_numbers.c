8fcdcde7492fb42f124e947e5256610ed38e4217
ifezue501 <flasysexy501@yahoo.com>
2022-10-20 12:36:57 -0700

    The numbers speak for themselves

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,7 +1,8 @@
 #include "main"
 
 /**
- * print_numbers - Prints the numbers from 0-9
+ * print_numbers - checks for checks for a digit (0 through 9)
+ *
  * Return: Always 0
  */
 void print_numbers(void)

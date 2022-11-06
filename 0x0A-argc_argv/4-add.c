0a088ba533d865382f97c0cda633da10c9df7e90
ifezue501 <flasysexy501@yahoo.com>
2022-11-06 08:37:30 -0800

    a program that adds positive numbers

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,83 +1,39 @@
 #include <stdio.h>
 #include "main.h"
+#include <stdlib.h>
 
 /**
- * _atoi - converts a string to an integer
- * @s: string to be converted
- *
- * Return: the int converted from the string
+ * main - add 2 positive numbers and print the result
+ * @argc: argument count
+ * @argv: argument vector, array of strings
+ * Description: If no number is passed to program, print 0.
+ * If one of the numbers contain non-digits, print Error.
+ * Return: 1 if error, 0 if function runs properly.
  */
-int _atoi(char *s)
-{
-	int i, d, n, len, f, digit;
 
-	i = 0;
-	d = 0;
-	n = 0;
-	len = 0;
-	f = 0;
-	digit = 0;
-
-	while (s[len] != '\0')
-		len++;
-
-	while (i < len && f == 0)
-	{
-		if (s[i] == '-')
-			++d;
-
-		if (s[i] >= '0' && s[i] <= '9')
-		{
-			digit = s[i] - '0';
-			if (d % 2)
-				digit = -digit;
-			n = n * 10 + digit;
-			f = 1;
-			if (s[i + 1] < '0' || s[i + 1] > '9')
-				break;
-			f = 0;
-		}
-		i++;
-	}
-
-	if (f == 0)
-		return (0);
-}
-
-/**
- * main - adds two positive number
- * @argc: number of arguments
- * @argv: array of arguents
- *
- * Return: 0 (Success), or 1 (Success)
- */
 int main(int argc, char *argv[])
 {
-	int sum, num, i, j, k;
+	int total, i, j;
+	int num;
 
-	sum = 0;
-
-	for (i = 1; i < argc; i++)
+	total = 0;
+	if (argc > 1)
 	{
-		for (j = 0; argv[i][j] != '\0'; j++)
+		for (i = 1; argv[i]; i++)
 		{
-			if (argv[i][j] > '9' || argv[i][j] < '0')
+			for (j = 0; argv[i][j] != '\0'; j++)
 			{
-				puts("Error");
-				return (1);
+				if (argv[i][j] < '0' || argv[i][j] > '9')
+				{
+					printf("Error\n");
+					return (1);
+				}
 			}
+
+			num = atoi(argv[i]);
+			total += num;
 		}
 	}
-
-	for (k = 1; k < argc; k++)
-	{
-		num = _atoi(argv[k]);
-		if (num >= 0)
-		{
-			sum += num;
-		}
-	}
-
-	printf("%d\n", sum);
+	printf("%d\n", total);
 	return (0);
 }

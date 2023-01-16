9da5530395237d396028bc17f84bb0f61026197b
ifezue501 <flasysexy501@yahoo.com>
2023-01-16 13:44:28 -0800

    Task 3

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 wget -P ../ https://raw.github.com/ifezue501/alx-
-low_level_programming/master/0x18-dynamic_libraries/libhack.so
+low_level_programming/tree/master/0x18-dynamic_libraries/libhack.so
 export LD_PRELOAD=../libhack.so
